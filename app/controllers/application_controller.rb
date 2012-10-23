999d17f6beed3506096903a0d6c4032937ddc49f
Luke Byrne <lukeb@lukebyrne.com>
2012-10-22 20:59:46 -0700

    Added front end and ability to login, logout etc etc.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+	def after_sign_in_path_for(resource)
+    stored_location_for(resource) ||
+     if resource.is_a?(User)
+       users_index_path
+     else
+       super
+     end
+  end
+
 end
